3c0c702474e04efa4439d5a4bd60e60e82ae8225
bchoii <bchoii@gmail.com>
2019-05-18 15:27:24 +0800

    .

--- src/robotjs.cc
+++ src/robotjs.cc
@@ -786,8 +786,8 @@ NAN_METHOD(getColor)
 	MMBitmapRef bitmap;
 	MMRGBHex color;
 
-	size_t x = info[1]->Int32Value();
-	size_t y = info[2]->Int32Value();
+	size_t x = info[1]->Int32Value(Nan::GetCurrentContext()).FromJust();
+	size_t y = info[2]->Int32Value(Nan::GetCurrentContext()).FromJust();
 
 	//Get our image object from JavaScript.
 	BMP img = buildBMP(Nan::To<v8::Object>(info[0]).ToLocalChecked());
