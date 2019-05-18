b0a243b0385b34646b3445bc36682a995ea6e6dd
bchoii <bchoii@gmail.com>
2019-05-18 15:26:09 +0800

    .

--- src/robotjs.cc
+++ src/robotjs.cc
@@ -141,7 +141,7 @@ NAN_METHOD(mouseClick)
 
 	if (info.Length() > 0)
 	{
-		v8::String::Utf8Value bstr(info[0]->ToString(Nan::GetCurrentContext()));
+		v8::String::Utf8Value bstr(info[0]->ToString());
 		const char * const b = *bstr;
 
 		switch (CheckMouseButton(b, &button))
